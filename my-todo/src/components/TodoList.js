eed7cba2b9bebf47c76af83d1e817423304cc8a1
Vera Zhao <yuwei.zhao@student.unsw.edu.au>
2022-03-23 18:49:45 +0800

    Todo Application without a good style, only js and html

---
+++ my-todo/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+// Importing Components
+import Todo from "./Todo";
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map(todo => (
+                    <Todo 
+                        text={todo.text}    
+                        key={todo.id}
+                        todo={todo} 
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
